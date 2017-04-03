2c044ce94ec7a557ef78d6a7062fb985d171c723
Ertron <ertron@veldrin.kiev.ua>
2017-04-03 19:16:06 +0300

    full project

--- src/components/header/index.js
+++
@@ -1,9 +0,0 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router';
-import style from './style';
-
-export default () => (
-	<header class={style.header}>
-		<h1>Test Preact App</h1>
-	</header>
-);
--- src/components/home/index.js
+++
@@ -1,9 +0,0 @@
-import { h, Component } from 'preact';
-import style from './style';
-
-export default () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
